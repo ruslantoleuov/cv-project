ab6b6afbe4960df031a12597af7a3569f1fdcd0b
Ruslan Toleuov <ruslantoleuov2015@gmail.com>
2022-06-30 19:07:36 +0500

    Add buttons for changing CV background color

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import CV from "./CV";
@@ -9,6 +9,10 @@ class Main extends Component {
     super(props);
     this.doc = new jsPDF("portrait", "pt", "a4");
     this.generatePDF = this.generatePDF.bind(this);
+    this.pointerEnterHandler = this.pointerEnterHandler.bind(this);
+    this.pointerLeaveHandler = this.pointerLeaveHandler.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+    this.colorButtonsRef = React.createRef();
   }
 
   generatePDF() {
@@ -23,6 +27,91 @@ class Main extends Component {
     });
   }
 
+  pointerEnterHandler(evt) {
+    if (!evt.target.classList.contains("color-buttons")) {
+      const target = getComputedStyle(evt.target);
+      const cv = document.querySelector(".cv");
+
+      for (const child of this.colorButtonsRef.current.children) {
+        child.classList.remove("active-color");
+      }
+
+      cv.style.backgroundColor = target.backgroundColor;
+    }
+  }
+
+  pointerLeaveHandler(evt) {
+    if (!evt.target.classList.contains("color-buttons")) {
+      const cv = document.querySelector(".cv");
+      cv.removeAttribute("style");
+
+      switch (true) {
+        case cv.classList.contains("color-red"):
+          this.colorButtonsRef.current.children[1].classList.add(
+            "active-color"
+          );
+          break;
+        case cv.classList.contains("color-green"):
+          this.colorButtonsRef.current.children[2].classList.add(
+            "active-color"
+          );
+          break;
+        case cv.classList.contains("color-blue"):
+          this.colorButtonsRef.current.children[3].classList.add(
+            "active-color"
+          );
+          break;
+        case cv.classList.contains("color-purple"):
+          this.colorButtonsRef.current.children[4].classList.add(
+            "active-color"
+          );
+          break;
+        default:
+          this.colorButtonsRef.current.children[0].classList.add(
+            "active-color"
+          );
+          break;
+      }
+    }
+  }
+
+  clickHandler(evt) {
+    if (!evt.target.classList.contains("color-buttons")) {
+      const cv = document.querySelector(".cv");
+
+      for (const child of this.colorButtonsRef.current.children) {
+        child.classList.remove("active-color");
+      }
+
+      switch (true) {
+        case evt.target.classList.contains("color-button-1"):
+          cv.className = "cv";
+          evt.target.classList.add("active-color");
+          break;
+        case evt.target.classList.contains("color-button-2"):
+          cv.className = "cv";
+          cv.classList.add("color-red");
+          evt.target.classList.add("active-color");
+          break;
+        case evt.target.classList.contains("color-button-3"):
+          cv.className = "cv";
+          cv.classList.add("color-green");
+          evt.target.classList.add("active-color");
+          break;
+        case evt.target.classList.contains("color-button-4"):
+          cv.className = "cv";
+          cv.classList.add("color-blue");
+          evt.target.classList.add("active-color");
+          break;
+        case evt.target.classList.contains("color-button-5"):
+          cv.className = "cv";
+          cv.classList.add("color-purple");
+          evt.target.classList.add("active-color");
+          break;
+      }
+    }
+  }
+
   render() {
     return (
       <main className="main">
@@ -33,6 +122,49 @@ class Main extends Component {
         >
           Save as PDF
         </button>
+        <div
+          onClick={this.clickHandler}
+          onPointerOver={this.pointerEnterHandler}
+          onPointerOut={this.pointerLeaveHandler}
+          ref={this.colorButtonsRef}
+          className="color-buttons"
+        >
+          <button
+            className="color-button color-button-1 active-color"
+            title="Slate"
+            type="button"
+          >
+            &nbsp;
+          </button>
+          <button
+            className="color-button color-button-2"
+            title="Red"
+            type="button"
+          >
+            &nbsp;
+          </button>
+          <button
+            className="color-button color-button-3"
+            title="Green"
+            type="button"
+          >
+            &nbsp;
+          </button>
+          <button
+            className="color-button color-button-4"
+            title="Blue"
+            type="button"
+          >
+            &nbsp;
+          </button>
+          <button
+            className="color-button color-button-5"
+            title="Purple"
+            type="button"
+          >
+            &nbsp;
+          </button>
+        </div>
         <CV />
       </main>
     );
