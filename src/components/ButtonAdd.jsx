efa8b07181d92aea69de7ccfdb5dc6ba8553b5c2
Ruslan Toleuov <ruslantoleuov2015@gmail.com>
2022-07-06 18:43:55 +0500

    Add ButtonAdd.jsx

---
+++ src/components/ButtonAdd.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+class ButtonAdd extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Button {...this.props} className="add-btn">
+        +
+      </Button>
+    );
+  }
+}
+
+export default ButtonAdd;
