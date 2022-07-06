b9883cd1557c3f6ef0280f6dba8a2f3cb0b0a5db
Ruslan Toleuov <ruslantoleuov2015@gmail.com>
2022-07-06 18:44:09 +0500

    Add ButtonRemove.jsx

---
+++ src/components/ButtonRemove.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+class ButtonRemove extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Button {...this.props}>x</Button>;
+  }
+}
+
+export default ButtonRemove;
