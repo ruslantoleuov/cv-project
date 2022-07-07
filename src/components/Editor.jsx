a4b090f115b38526cba3e323bd94eaec8ea5ac56
Ruslan Toleuov <ruslantoleuov2015@gmail.com>
2022-07-07 17:23:17 +0500

    Edit Editor.jsx, minor change

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { Component } from "react";
 import InputField from "./InputField";
 import TextArea from "./TextArea";
 import ButtonAdd from "./ButtonAdd";
